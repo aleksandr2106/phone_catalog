9e281cd478bb7a036dfd5a99192e8beb9d32bae9
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-02-21 22:43:24 +0200

    phone_list_fixed_problem_with_img

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -4,15 +4,19 @@ import "./ProductItem.css";
 class ProductItem extends Component {
   render() {
     const { phone_img, name, snippet } = this.props;
-    var phone_img_link = "../";
+    var phone_img_link = "http://localhost:3000/";
     phone_img_link += phone_img;
     console.log(phone_img_link);
 
     return (
       <div className="product_item">
         <img className="product_item_img" src={phone_img_link} alt={name} />
-        <h3 className="product_item_name">{name}</h3>
-        <p className="product_item_snippet">{snippet}</p>
+        <div className="info_about_phone">
+          <a href="#" className="product_item_name">
+            {name}
+          </a>
+          <p className="product_item_snippet">{snippet}</p>
+        </div>
       </div>
     );
   }
