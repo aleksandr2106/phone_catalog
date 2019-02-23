e5773fb0bf1c7be922d56193638d2c22c9e08910
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-02-23 23:55:14 +0200

    Filter_add

--- src/components/ProductListView/ProductListView.js
+++ src/components/ProductListView/ProductListView.js
@@ -5,16 +5,74 @@ import './ProductListView.css';
 var phones = require('./phones.json'); // forward slashes will depend on the file location
 
 class ProductList extends Component {
+  state = {
+    inputPhoneQuery: '',
+    sortMethod: 'newest',
+  };
+
+  replaceSortMethod = e => {
+    this.setState({ sortMethod: e.target.value });
+  };
+
+  sortItems = (first, second) => {
+    switch (this.state.sortMethod) {
+      case 'alphabetical': {
+        return (first.name > second.name) - (first.name < second.name);
+      }
+      case 'reverseAlphabetical': {
+        return (first.name < second.name) - (first.name > second.name);
+      }
+      case 'newest': {
+        return first.age - second.age;
+      }
+    }
+  };
   render() {
     return (
       <div className="product_list_view">
-        {phones.map(phoneList => (
-          <ProductItem
-            imageUrl={phoneList.imageUrl}
-            name={phoneList.name}
-            snippet={phoneList.snippet}
-          />
-        ))}
+        <div className="product_filter">
+          <div className="input_filter">
+            <p>Search </p>
+            <input
+              type="text"
+              onChange={e => this.setState({ inputPhoneQuery: e.target.value })}
+              value={this.state.inputPhoneQuery}
+              placeholder="Enter phone name"
+            />
+          </div>
+          <div className="product_sort">
+            <p>Sort </p>
+            <select
+              value={this.state.sortMethod}
+              onChange={this.replaceSortMethod}
+            >
+              <option value="newest">Newest</option>
+              <option value="alphabetical">Alphavite</option>
+              <option value="reverseAlphabetical">Reverse Alphavite</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="product_list">
+          {phones
+            .sort(this.sortItems)
+            .filter(
+              phoneList =>
+                phoneList.name
+                  .toLowerCase()
+                  .includes(this.state.inputPhoneQuery.toLowerCase()) ||
+                phoneList.snippet
+                  .toLowerCase()
+                  .includes(this.state.inputPhoneQuery.toLowerCase())
+            )
+            .map(phoneList => (
+              <ProductItem
+                imageUrl={phoneList.imageUrl}
+                name={phoneList.name}
+                snippet={phoneList.snippet}
+              />
+            ))}
+        </div>
       </div>
     );
   }
